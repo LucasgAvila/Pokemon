8599aab46a0e91a66c484f1abcd151caf44943be
Lucas Avila <lucasg.avilas@gmail.com>
2021-12-14 00:16:50 -0300

    Comienzo de configuacion para favoritos

---
+++ src/components/Pokemon/Favorite.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Icon from "react-native-vector-icons/FontAwesome5"
+
+export default function Favorite(props) {
+    const {id} = props;
+    const addFavorite = () => {
+            console.log('añadir a favorito', id)
+    }
+    
+    return (
+       <Icon 
+       name='heart' 
+       color="#fff" 
+       size={20} 
+       onPress={addFavorite} 
+       style={{marginRight: 20}}   />
+    )
+}
--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -5,6 +5,8 @@ import { getPokemonDetailsApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
 import Stats from "../components/Pokemon/Stats";
+import Favorite from "../components/Pokemon/Favorite";
+import useAuth from '../hooks/userAuth'
 
 export default function Pokemon(props) {
   const {
@@ -13,9 +15,11 @@ export default function Pokemon(props) {
   } = props;
   const [pokemon, setPokemon] = useState(null);
 
+  const {auth} = useAuth();
+
   useEffect(() => {
     navigation.setOptions({
-      headerRight: () => null,
+      headerRight: () => auth && <Favorite id={pokemon?.id} />, 
       headerLeft: () => (
         <Icon
           name="arrow-left"
@@ -26,7 +30,7 @@ export default function Pokemon(props) {
         />
       ),
     });
-  }, [navigation, params]);
+  }, [navigation, params, pokemon]);
 
   useEffect(() => {
     (async () => {
