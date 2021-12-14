71a03a4d8ba9692c3dddb5c1be7ab4fa46b0eef6
Lucas Avila <lucasg.avilas@gmail.com>
2021-12-14 18:24:44 -0300

    Finalizado de la parte Favoritos

--- src/components/Auth/UserData.js
+++ src/components/Auth/UserData.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState, useCallback} from 'react'
 import {StyleSheet, View, Text, Button } from 'react-native'
+import {useFocusEffect} from '@react-navigation/native'
+import { size } from 'lodash'
 import useAuth from '../../hooks/userAuth'
+import {getPokemonFavoriteApi} from '../../api/favorite'
 
 export default function UserData() {
     const { auth, logout } = useAuth();
+    const [total, setTotal] = useState(0);
+
+    useFocusEffect(
+        useCallback(() => {
+        (async()=> {
+            try {
+              const response = await getPokemonFavoriteApi();
+              setTotal(size(response));
+            } catch (error) {
+                setTotal(0)
+            }
+        })()    
+
+            }, [])
+    )
+
+
     return (
         <View style={styles.content}>
             <View style={styles.titleBlog}> 
@@ -16,7 +36,7 @@ export default function UserData() {
                 <ItemMenu title="Nombre" text={`${auth.firstName} ${auth.lastName}`} />
                 <ItemMenu title='Username' text={auth.username}  />
                 <ItemMenu title='Email' text={auth.email} />
-                <ItemMenu  title="Total favoritos" text={`0 pokemons`} />
+                <ItemMenu  title="Total favoritos" text={`${total} pokemons`} />
             </View>
             <Button title="Desconectarse" onPress={logout} style={styles.btnLogout} />
         </View>
--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,10 +1,45 @@
-import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import React, { useState,  useCallback } from 'react'
+import { Text } from 'react-native'
+import { useFocusEffect} from '@react-navigation/native'
+import { getPokemonFavoriteApi  } from '../api/favorite'
+import useAuth from '../hooks/useAuth'
+import {getPokemonDetailsApi} from '../api/pokemon'
+import PokemonList from '../components/PokemonList'
+import NoLogged from '../components/NoLogged'
 
 export default function Favorite() {
-    return (
-        <SafeAreaView>
-            <Text>Favorite</Text>
-        </SafeAreaView>
+ const [pokemons, setPokemon] = useState([])
+ const { auth } = useAuth();
+
+ useFocusEffect(
+    useCallback(() => {
+        if(auth){
+         (async () => {
+             const response = await get.getPokemonFavoriteApi();
+     
+             const pokemonsArray = [];
+           for await (const pokemon of response) {
+             const pokemonDetails = await getPokemonDetailsApi(id);
+     
+              pokemonsArray.push({
+                id: pokemonDetails.id,
+                name: pokemonDetails.name,
+                type: pokemonDetails.types[0].type.name,
+                order: pokemonDetails.order,
+                image:
+                  pokemonDetails.sprites.other["official-artwork"].front_default,
+              });
+           }
+     
+           setPokemon(pokemonsArray)
+         })();
+        }
+      }, [auth]) 
+ )
+
+ 
+ 
+ return (
+       !auth ? <NoLogged /> : <PokemonList pokemons={pokemons} />
     )
 }
